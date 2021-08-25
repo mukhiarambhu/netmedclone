88903ee432a0f446342cebeba0a9f9cb3e0af3bc
rambhumukhia <email@masaischool.com>
2021-08-25 23:59:04 +0530

    discountadded

--- cart.js
+++ cart.js
@@ -1,3 +1,13 @@
+//providing sticky part
+
+window.addEventListener("scroll", function () {
+  var navbar = document.getElementById("divPanel1");
+
+  if (window.pageYOffset == 0) {
+    navbar.classList.add("sticky");
+  }
+});
+
 //slider1 code
 
 let slider1_image = [
@@ -34,12 +44,106 @@ slider();
 
 // slider 1 end
 
-//providing sticky part
+//append product on div
 
-window.addEventListener("scroll", function () {
-  var navbar = document.getElementById("divPanel1");
+//div where product will be displayed
+var display_diV_cart = document.getElementById("displayCartProduct");
 
-  if (window.pageYOffset == 0) {
-    navbar.classList.add("sticky");
+//function to append product
+function showProduct(el) {
+  let productcarddiv = document.createElement("div"); //main div
+
+  let productInfoDiv = document.createElement("div"); //product information
+
+  let name = document.createElement("h5");
+  name.innerText = el.name;
+
+  let manufacturer = document.createElement("p");
+  manufacturer.innerText = el.brand;
+
+  let price = document.createElement("p");
+  price.innerText = `Rs. ${el.price}`;
+
+  let deliverydate = document.createElement("p");
+  deliverydate.innerText = "Delivery between SEPTEMBER 2-SEPTEMBER 5";
+
+  productInfoDiv.append(name, manufacturer, price, deliverydate); //appending product info to infodiv
+
+  //div to append image
+
+  let image_div = document.createElement("div");
+
+  let image = document.createElement("img");
+  image.src = el.image;
+
+  image_div.append(image);
+
+  //horizontalline
+  let horizontalline = document.createElement("hr");
+
+  //append to main div
+  productcarddiv.append(image_div, productInfoDiv);
+
+  //append to dom
+
+  display_diV_cart.append(productcarddiv, horizontalline);
+}
+
+var cartValue = 0;
+var product_incart = 0;
+
+function addproduct(el) {
+  //get data fromcart
+  let cart_data = JSON.parse(localStorage.getItem("cart"));
+
+  //loop through cartdata to get access to individual product
+
+  cart_data.forEach(function (el) {
+    showProduct(el);
+    product_incart++;
+    cartValue += Number(el.price);
+  });
+}
+addproduct();
+
+//display total product in cart on nav bar
+
+var no_of_product_display = document.getElementById("carttoal");
+
+var total_product_p = document.createElement("p");
+total_product_p.innerText = product_incart;
+no_of_product_display.append(total_product_p);
+
+var Total_price_display = document.getElementById("mrptotal");
+Total_price_display.append(`Rs.${cartValue}`);
+
+// discount
+
+function applyDiscount() {
+  var promo_value = document.getElementById("promocode").value;
+
+  let price = cartValue;
+  totalitem = product_incart;
+  var discount = 0;
+  if (promo_value == "med30") {
+    discount += Math.round((30 / 100) * price);
   }
-});
+
+  var discount_display = document.getElementById("meddiscountdisplay");
+  document.getElementById("meddiscountdisplay").innerHTML = null;
+  discount_display.append(`Rs.${discount}`);
+
+  let totalamount = price - discount;
+  let total_display = document.getElementById("totalamountdisplay");
+  document.getElementById("totalamountdisplay").innerHTML = null;
+  total_display.append(`Rs.${totalamount}`);
+
+  let total_display1 = document.getElementById("totalamountdisplay1");
+  document.getElementById("totalamountdisplay1").innerHTML = null;
+  total_display1.append(`Rs.${totalamount}`);
+
+  var totalsaving_display = document.getElementById("totalsaving_display");
+  totalsaving_display.append(`TOTAL SAVING Rs.${discount}`);
+
+  document.getElementById("promocode").value = null;
+}
